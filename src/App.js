a9094922f67a98dadc7262883cfe74eaafb1f607
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-12-04 20:20:00 +0300

    graphql is playground

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div>
+import { useQuery, gql } from "@apollo/client";
 
-    </div>
-  );
+const GET_ALL_POSTS = gql`
+    query posts {
+        posts {
+            title
+            description
+        }
+    }
+`;
+
+function App() {
+    const { loading, error, data } = useQuery(GET_ALL_POSTS);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return <>{JSON.stringify(data.posts)}</>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+    uri: process.env.REACT_APP_GRAPHQL_API,
+    cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </ApolloProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
